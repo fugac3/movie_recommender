4caeaf322d3ef96298733a907bc63a782e107911
Hope <k.hope.ing@gmail.com>
2026-05-04 19:17:51 +0200

    Added tmdb genre mapping and creating movie object functions

--- server.js
+++ server.js
@@ -23,3 +23,42 @@ app.use(express.static(path.join(__dirname))); //serve files in current dir
 app.listen(PORT, () => {
   console.log(`Movie recommender running at http://localhost:${PORT}`);
 });
+
+//Creates and returns movie object using only necessary details for frontend
+function shapeMovie(movie, genreMap) {
+  //Get genre names (from id to genre name) for movie using genreMap function
+  const genreNames = (movie.genre_ids || [])
+    .map((id) => genreMap.get(id)) // map through genre ids for movie and return genre name from genreMap
+    .filter(Boolean); // filter out any undefined values (in case genre id doesn't exist in genreMap)
+
+  return {
+    title: movie.title,
+    year: (movie.release_date || "").slice(0, 4) || "Unknown", //get year from release date (only 4 char eg. '1994')
+    genres: genreNames.length ? genreNames.join(", ") : "Unknown", //join genre names if more than 0 or 1 with , (ternary operator)
+    rating: movie.vote_average, // 0-10 scale, frontend converts to stars
+    description: movie.overview || "No description available.",
+    poster: movie.poster_path ? `${IMG_BASE}${movie.poster_path}` : null,
+  };
+}
+
+//getgenreMap Fetches the {id -> name} genre map from TMDB. Cached in memory after because genres stay sam3
+let genreMapCache = null;
+async function getGenreMap() {
+  if (genreMapCache) return genreMapCache; //if genreMapCache not null, exit function
+
+  const url = `${TMDB_BASE}/genre/movie/list?api_key=${TMDB_API_KEY}&language=en-US`; //url to fetch genre list from TMDB
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`TMDB genre list failed: ${response.status}`);
+  }
+  const data = await response.json();
+
+  //data.genres returns [{id: 28, name: "Action"}, {id: 12, name: "Adventure"}, ...]
+  genreMapCache = new Map(data.genres.map((g) => [g.id, g.name])); //for every item in genres array insert id and name of genre in genreMapCache
+
+  console.log(genreMapCache); //(test to see if runs properly)
+
+  return genreMapCache;
+}
+
+getGenreMap();
